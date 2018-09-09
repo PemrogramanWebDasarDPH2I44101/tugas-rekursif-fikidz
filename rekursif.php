9794124b03f89b2a4fc53422f2166f13ad6fbad9
Fiki Dzulfiqar <33850678+fikidz@users.noreply.github.com>
2018-09-09 14:39:20 +0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -1,10 +1,38 @@
-<?php
-
-function rekursif($i){
-  echo "$i <br>";
-  $i--;
-  if($i>0)
-    rekursif($i);
+<?php 
+error_reporting(0);
+$angka = 5;
+function pertama($x, $y){
+	if ($x > 0) {
+		if ($y > 0) {
+			echo "* ";
+			$y--;
+			pertama($x, $y);
+		}
+		else{
+			echo "<br>";
+			$x--;
+			pertama($x, $x);
+		}
+	}
 }
-rekursif(5);
-?>
+pertama ($angka, $angka);
+
+echo "========================<br>";
+
+function kedua($x, $y, $angka){
+	if ($x <= $angka) {
+		if ($y <= $x) {
+			echo "$y ";
+			$y++;
+			kedua($x, $y, $angka);
+		}
+		else{
+			echo "<br>";
+			$x++;
+			kedua($x, 1, $angka);
+		}
+	}
+}
+kedua (1, 1, $angka);
+
+ ?>
